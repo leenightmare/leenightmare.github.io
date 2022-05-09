1a57fc5bb8a8d45a53328e1ab3ce1977ecc6e35a
lijad <1518674973@qq.com>
2022-05-09 22:35:23 +0800

    auto commit

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 var lisconfig = {
-  username: 'lijad',
+  username: 'leenightmare',
   clientID: '7387af928abb36c3b267',
   clientSecret: 'd27e312fdc70bd13a69b4aefd00914a88b554632',
   // record: '粤ICP备xxxxxxxxxx号',
